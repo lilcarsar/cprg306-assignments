ffd8f3193855b167d16bcd95907fd2bac0c96af0
Carter <cdsarney@gmail.com>
2023-09-14 14:33:32 -0600

    Initial commit

---
+++ app/mycomp.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+
+export default function MyComp() {
+    return (
+        <main>
+            <h2>My component</h2>
+            <Link href="https://www.google.ca">Google</Link>
+        </main>
+    )
+    }
---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import MyComp from './mycomp'
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Hello World!!!!!</h1>
+      <MyComp />
+      
+    </main>
+  )
+}
